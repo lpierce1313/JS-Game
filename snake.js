051d879e547cce8060a2ce0b0363749a57f62ec6
Luke Pierce <lpierce1313@gmail.com>
2018-10-09 16:19:07 -0600

    Updated game over and play screens

--- snake.js
+++ snake.js
@@ -5,6 +5,11 @@ SNAKE_BLOCK_SIZE = 20;
 
 canvas = null;
 ctx = null;
+gameState = 0;
+score = 0;
+failed = 1;
+x = 0;
+y = 0;
 
 window.onload = function() {
 	canvas = document.getElementById("canvas");
@@ -40,36 +45,121 @@ window.onload = function() {
 }
 
 function gameLoop() {
-	draw();
+	if(gameState == 0){
 
-	for(var i = 0; i < snakeBlocks.length; i++) {
-		for(var j = 0; j < changes.length; j++) {
-			if(snakeBlocks[i].x === changes[j].x && snakeBlocks[i].y === changes[j].y) {
-				snakeBlocks[i].dx = changes[j].dx;
-				snakeBlocks[i].dy = changes[j].dy;
-				if(i === snakeBlocks.length - 1) changes.shift();
+		clear();
+		ctx.font = "80px Courier New";
+		ctx.fillStyle = "#00ff00";
+		ctx.textAlign = "center";
+		ctx.fillText("SnakeBlocks", canvas.width/2, 150);
+
+		ctx.font = "40px Courier New";
+		ctx.fillStyle = "#f00";
+		ctx.textAlign = "center";
+		ctx.fillText("Play Game", canvas.width/2, 290);
+
+		ctx.beginPath();
+		ctx.lineWidth="3";
+		ctx.strokeStyle="white";
+		ctx.rect(475, 230, 250, 100);
+		ctx.stroke();
+
+		window.onmousedown = function(e){
+			var x = event.x;
+			var y = event.y;
+			x -= canvas.offsetLeft;
+			y -= canvas.offsetTop;
+			y += Math.round(window.scrollY);
+			alert("x:" + x + " y:" + y);
+			ctx.rect(475, 230, 250, 100);
+			if(x > 475 && x < 475 + 250 && y > 230 && y < 230 + 100){
+				gameState = 1;
 			}
 		}
-		snakeBlocks[i].x += snakeBlocks[i].dx * SNAKE_BLOCK_SIZE;
-		snakeBlocks[i].y += snakeBlocks[i].dy * SNAKE_BLOCK_SIZE;
+	}
+	else if(gameState == 1){
+		clear();
+		draw();
+		for(var i = 0; i < snakeBlocks.length; i++) {
+			for(var j = 0; j < changes.length; j++) {
+				if(snakeBlocks[i].x === changes[j].x && snakeBlocks[i].y === changes[j].y) {
+					snakeBlocks[i].dx = changes[j].dx;
+					snakeBlocks[i].dy = changes[j].dy;
+					if(i === snakeBlocks.length - 1) changes.shift();
+				}
+			}
+			snakeBlocks[i].x += snakeBlocks[i].dx * SNAKE_BLOCK_SIZE;
+			snakeBlocks[i].y += snakeBlocks[i].dy * SNAKE_BLOCK_SIZE;
+		}
+	}
+	else{
+		clear();
+		if(failed == 1){
+			var replay = "Try Again";
+			var status = "You Suck!"
+		}
+		else{
+			var replay = "Play Again";
+			var status = "You Did Great!"
+		}
+
+		ctx.font = "80px Courier New";
+		ctx.fillStyle = "#00ff00";
+		ctx.textAlign = "center";
+		ctx.fillText("Game Over", canvas.width/2, 150);
+
+		ctx.font = "32px Courier New";
+		ctx.fillStyle = "#00ff00";
+		ctx.textAlign = "left";
+		ctx.fillText(status, 20, 430);
+		ctx.fillText("Score: 4000", 20, 480);
+
+		ctx.font = "40px Courier New";
+		ctx.fillStyle = "#f00";
+		ctx.textAlign = "center";
+		ctx.fillText(replay, canvas.width/2, 290);
+
+		ctx.beginPath();
+		ctx.lineWidth="3";
+		ctx.strokeStyle="white";
+		ctx.rect(475, 230, 250, 100);
+		ctx.stroke();
+
+		window.onmousedown = function(e){
+			var x = event.x;
+			var y = event.y;
+			x -= canvas.offsetLeft;
+			y -= canvas.offsetTop;
+			y += Math.round(window.scrollY);
+			alert("x:" + x + " y:" + y);
+			ctx.rect(475, 230, 250, 100);
+			if(x > 475 && x < 475 + 250 && y > 230 && y < 230 + 100){
+				gameState = 1;
+			}
+		}
 	}
 }
 
 function draw() {
-	ctx.clearRect(0, 0, canvas.width, canvas.height);
 
 	for(var i = 0; i < snakeBlocks.length; i++) {
+		ctx.fillStyle = "#00ff00";
 		ctx.fillRect(snakeBlocks[i].x, snakeBlocks[i].y, 0.9* SNAKE_BLOCK_SIZE, 0.9* SNAKE_BLOCK_SIZE);
 	}
 }
 
 function appendBlock() {
 	var lastBlock = snakeBlocks[snakeBlocks.length - 1];
-	
+
 	snakeBlocks.push({
 		x: lastBlock.x - lastBlock.dx * SNAKE_BLOCK_SIZE,
 		y: lastBlock.y - lastBlock.dy * SNAKE_BLOCK_SIZE,
 		dx: lastBlock.dx,
 		dy: lastBlock.dy
 	});
+}
+
+function clear(){
+	ctx.fillStyle = "#000";
+	ctx.fillRect(0, 0, canvas.width, canvas.height);
 }
