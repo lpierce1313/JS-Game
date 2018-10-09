9844b22fda8840dd7a1e17d0a8b2564413ede452
Luke Pierce <lpierce1313@gmail.com>
2018-10-09 16:27:12 -0600

    updated_js

--- snake_old.js
+++ snake_old.js
@@ -1,18 +1,10 @@
-// Constants
-SNAKE_BLOCK_SIZE = 20;
-NUM_SOLID_BLOCKS = 3;
-
-// Game items
 snakeBlocks = [];
 changes = [];
-solidBlocks = [];
-pellet = {};
 
-// Canvas stuff
+SNAKE_BLOCK_SIZE = 20;
+
 canvas = null;
 ctx = null;
-
-//Game stats
 gameState = 0;
 score = 0;
 failed = 1;
@@ -23,10 +15,13 @@ window.onload = function() {
 	canvas = document.getElementById("canvas");
 	ctx = canvas.getContext("2d");
 
-	initGame();
+	snakeBlocks.push({x: 200, y: 200, dx: 1, dy: 0});
 
 	window.onkeydown = function(e) {
 		switch(e.keyCode) {
+			case 32: // SPACE
+				appendBlock();
+				break;
 			case 37: // LEFT
 				changes.push({x: snakeBlocks[0].x, y: snakeBlocks[0].y, dx: -1, dy: 0});
 				e.preventDefault();
@@ -46,20 +41,12 @@ window.onload = function() {
 		}
 	}
 
-	setInterval(gameLoop, 100);
-}
-
-function initGame() {
-	snakeBlocks = [];
-	snakeBlocks.push({x: 200, y: 200, dx: 1, dy: 0});
-
-	randomizePellet();
-
-	randomizeSolidBlocks(3);
+	setInterval(gameLoop, 150);
 }
 
 function gameLoop() {
 	if(gameState == 0){
+
 		clear();
 		ctx.font = "80px Courier New";
 		ctx.fillStyle = "#00ff00";
@@ -94,35 +81,15 @@ function gameLoop() {
 		clear();
 		draw();
 		for(var i = 0; i < snakeBlocks.length; i++) {
-			// change the tail direction at the correct point
 			for(var j = 0; j < changes.length; j++) {
 				if(snakeBlocks[i].x === changes[j].x && snakeBlocks[i].y === changes[j].y) {
 					snakeBlocks[i].dx = changes[j].dx;
 					snakeBlocks[i].dy = changes[j].dy;
-
-					// Remove from the changes after all the blocks have gone through it
 					if(i === snakeBlocks.length - 1) changes.shift();
 				}
 			}
-
 			snakeBlocks[i].x += snakeBlocks[i].dx * SNAKE_BLOCK_SIZE;
 			snakeBlocks[i].y += snakeBlocks[i].dy * SNAKE_BLOCK_SIZE;
-
-			// If we collide with our tail, GAME OVER
-			if(i != 0 && snakeBlocks[0].x == snakeBlocks[i].x && snakeBlocks[0].y == snakeBlocks[i].y) {
-				// GAME OVER
-				initGame();
-				return;
-			}
-
-			// Check for collision with solid blocks
-			for(var j = 0; j < solidBlocks.length; j++) {
-				if(snakeBlocks[i].x === solidBlocks[j].x && snakeBlocks[i].y === solidBlocks[j].y) {
-					// GAME OVER
-					initGame()
-					return;
-				}
-			}
 		}
 	}
 	else{
@@ -171,39 +138,13 @@ function gameLoop() {
 			}
 		}
 	}
-
-	// Game over if we hit the edges of the canvas
-	if(snakeBlocks[0].x < 0 || snakeBlocks[0].x >= canvas.width || snakeBlocks[0].y < 0 || snakeBlocks[0].y >= canvas.height) {
-		initGame();
-		return;
-	}
-
-	// Check collision with pellet
-	if(snakeBlocks[0].x === pellet.x && snakeBlocks[0].y === pellet.y) {
-		appendBlock();
-		randomizeSolidBlocks(solidBlocks.length + 1);
-		randomizePellet();
-	}
 }
 
 function draw() {
-	ctx.fillStyle = "#000000";
-	ctx.fillRect(0, 0, canvas.width, canvas.height);
 
-	// Draw pellet
-	ctx.fillStyle = "#ff0000";
-	ctx.fillRect(pellet.x, pellet.y, SNAKE_BLOCK_SIZE, SNAKE_BLOCK_SIZE);
-
-	// Draw snake
 	for(var i = 0; i < snakeBlocks.length; i++) {
 		ctx.fillStyle = "#00ff00";
-		ctx.fillRect(snakeBlocks[i].x, snakeBlocks[i].y, 0.99 * SNAKE_BLOCK_SIZE, 0.99 * SNAKE_BLOCK_SIZE);
-	}
-
-	// Draw solid blocks
-	for(var i = 0; i < solidBlocks.length; i++) {
-		ctx.fillStyle = "#ffff00";
-		ctx.fillRect(solidBlocks[i].x, solidBlocks[i].y, SNAKE_BLOCK_SIZE, SNAKE_BLOCK_SIZE);
+		ctx.fillRect(snakeBlocks[i].x, snakeBlocks[i].y, 0.9* SNAKE_BLOCK_SIZE, 0.9* SNAKE_BLOCK_SIZE);
 	}
 }
 
@@ -216,22 +157,6 @@ function appendBlock() {
 		dx: lastBlock.dx,
 		dy: lastBlock.dy
 	});
-}
-
-function randomBoardPos(max) {
-	return Math.floor((max / SNAKE_BLOCK_SIZE) * Math.random()) * SNAKE_BLOCK_SIZE
-}
-
-function randomizeSolidBlocks(numBlocks) {
-	if(numBlocks > 10) numBlocks = 10;
-	solidBlocks = [];
-	for(var i = 0; i < numBlocks; i++) {
-		solidBlocks.push({x: randomBoardPos(canvas.width), y: randomBoardPos(canvas.height)});
-	}
-}
-
-function randomizePellet() {
-	pellet = {x: randomBoardPos(canvas.width), y: randomBoardPos(canvas.height)};
 }
 
 function clear(){
